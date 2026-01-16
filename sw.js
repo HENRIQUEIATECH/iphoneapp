4e738d5098d3d695ee48582305fe6a9111f608b3
HENRIQUEIATECH <paulodallas63@gmail.com>
2026-01-16 17:33:22 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'rba-v2';
-const OFFLINE_URL = 'offline.html';
+const OFFLINE_URL = 'minijogo.html';
 
 // Instala e guarda a página offline no cache do celular
 self.addEventListener('install', (event) => {
